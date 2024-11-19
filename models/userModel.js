041f80b6298455f51601d3a0326ac0d9fad19eaa
Dilshan G.A.M <93402921+DilshanGAM@users.noreply.github.com>
2024-11-19 23:58:19 +0530

    privileges list sql query implementaion 👑

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,18 @@
-import { query } from '../config/database.js';
+import { query } from "../config/database.js";
 
 export const findUserByEmail = async (email) => {
-    const sql = 'SELECT * FROM user WHERE email = ?';
-    const users = await query(sql, [email]);
-    return users[0];
+  const sql = "SELECT * FROM user WHERE email = ?";
+  const users = await query(sql, [email]);
+  return users[0];
+};
+
+export const getPrivilegesList = async (email) => {
+  const sql = `SELECT rp.permission
+    FROM user u
+    JOIN role_permissions rp ON u.role_name = rp.role_name
+    WHERE u.email = ?;
+    `;
+  const privileges = await query(sql, [email]);
+  const privilegesList = privileges.map((privilege) => privilege.permission);
+  return privilegesList;
 };
